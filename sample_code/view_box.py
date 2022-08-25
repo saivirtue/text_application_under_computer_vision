2fe82b752fdf6ae6e869f9a68d8d4b0fcca8962b
Sam Cho <sam@is-land.com.tw>
2022-08-25 11:25:38 +0800

    upload sample images

---
+++ sample_code/view_box.py
@@ -0,0 +1,14 @@
+import argparse
+
+import cv2
+
+
+def main():
+    
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    ap.add_argument("--box", required=True, help="你從paddleocr取得的結果")
+
+    main()
